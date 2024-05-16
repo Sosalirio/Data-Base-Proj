2d604641e296b98e6e50f110b9aae0c18bc975c7
Ariel Cruz Sosa <102941282+Sosalirio@users.noreply.github.com>
2024-05-15 23:29:13 -0400

    Update index6.php

---
+++ DataBaseProject/index6.php
@@ -0,0 +1,54 @@
+<?php include("DBLogin.php")?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Data Base</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <div class="container">
+      <h1 class="title">My SQL Database by Ariel Cruz</h1>
+      <div class="sql-input">
+          <input type="text" placeholder="Enter SQL queries...">
+      </div>
+      <button onclick="document.location = 'index.html'"> Home</button>
+  </div>
+  <script src="script.js"></script>
+</body>
+
+  <body>
+    <section>
+        <h1>Pokemon Tables</h1>
+
+        <table>
+            <tr>
+                <th>EGID</th>
+                <th>EggGroup</th>
+              
+            </tr>
+
+            <?php 
+
+                while($rows=$result->fetch_assoc())
+                {
+            ?>
+            <tr>
+
+                <td><?php echo $rows['EGID'];?></td>
+                <td><?php echo $rows['EggGroup'];?></td>
+               
+            <?php
+                }
+            ?>
+        </table>
+    </section>
+  </body>
+
+  </html>
+
+
+</html>
